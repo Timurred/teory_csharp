7b6db42a200fbfc0660e726a3c9c82657eb4ad8e
Yumaguen Timur <timur_red82@mail.ru>
2024-02-16 08:51:46 +0300

    add Less1.task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -22,4 +22,11 @@ Console.WriteLine(product1);
 Console.WriteLine(product2);
 Console.WriteLine(product3);
 
+int quotient = i1/i2;
+Console.WriteLine(quotient);
+// целочисленное не может содержать дробную часть
+
+d1 = 6;
+double quotient2 = i1/d1;
+Console.WriteLine(quotient2);
 
